f7a4c2dcd67cb662d530b188b97339ecf7868a62
Ivan <iva.drakon.nov@gmail.com>
2020-07-09 08:56:38 +0200

    header topline

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,12 +2,22 @@
   <section class="header">
     <div class="wrapper">
       <div class="header__topline">
-        <div class="header__topline-logo">
-          <div>D</div>
+        <div class="header__topline-leftblock">
+          <div class="header__topline-logo">
+            <div>D</div>
+          </div>
+          <div class="header__topline-brand">
+            Daisy
+          </div>
         </div>
-        <div class="header__topline-brand">
-          Daisy
-        </div>
+        <nav class="header__topline-menu">
+          <ul class="header__topline-list">
+            <li class="header__topline-item">about</li>
+            <li class="header__topline-item">portfolio</li>
+            <li class="header__topline-item">services</li>
+            <li class="header__topline-item">contact</li>
+          </ul>
+        </nav>
       </div>
     </div>
   </section>
@@ -30,6 +40,7 @@ export default {
   background-size: 100% 99.9%;
   &__topline {
     display: flex;
+    justify-content: space-between;
     padding-top: 50px;
     &-logo {
       .hexogonlogo(#fc635e, #0c252d, 59px, 68px);
@@ -43,6 +54,27 @@ export default {
       font-family: "Gotham Pro";
       margin-left: 20px;
       margin-top: 10px;
+    }
+    &-leftblock {
+      display: flex;
+      flex-grow: 1;
+    }
+    &-menu {
+      flex-grow: 1;
+    }
+    &-list {
+      display: flex;
+      justify-content: flex-end;
+    }
+    &-item {
+      list-style: none;
+      text-transform: uppercase;
+      margin: -15px 45px 0 0;
+      color: #fff;
+      font-size: 16px;
+      letter-spacing: 1px;
+      line-height: 36px;
+      font-family: "P FEncore Sans Pro";
     }
   }
 }
