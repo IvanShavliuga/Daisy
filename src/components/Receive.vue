2884f94188e186bf70d9a55edd87d4e3c28d49e7
Ivan <iva.drakon.nov@gmail.com>
2020-07-09 22:20:28 +0200

    receive titles

---
+++ src/components/Receive.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="receive">
+    <div class="wrapper">
+      <h2 class="receive__title">
+        What can
+        <span class="receive__title-lastword">you receive?</span>
+      </h2>
+      <h3 class="receive__subtitle">
+        Daisy is sagittis sem nibh id elit. Duis sed odio sit amet nibh
+      </h3>
+    </div>
+  </section>
+</template>
+<style lang="less">
+.receive {
+  &__title {
+    font-size: 28px;
+    letter-spacing: 0px;
+    line-height: 26px;
+    color: @bodycolor;
+    font-weight: 500;
+    font-family: @bodyfont;
+    text-align: center;
+    text-transform: uppercase;
+    &-lastword {
+      color: @itemactive;
+    }
+  }
+  &__subtitle {
+    font-family: @bodyfont;
+    height: 51px;
+    font-size: 20px;
+    letter-spacing: 0px;
+    line-height: 26px;
+    color: #adadad;
+    font-weight: 300;
+    font-family: "P FEncore Sans Pro";
+    text-align: center;
+  }
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,24 @@
 <template>
+<main>
   <Header/>
+  <Receive/>
+</main>
 </template>
+<style>
+main {
+  margin: 0;
+  padding: 0;
+}
+</style>
 <script>
 // @ is an alias to /src
 import Header from '@/components/Header.vue'
+import Receive from '@/components/Receive.vue'
 export default {
   name: 'Home',
   components: {
-    Header
+    Header,
+    Receive
   }
 }
 </script>
