0b35ed3e155b0283de6a8928084c88f6785171cf
Ivan <iva.drakon.nov@gmail.com>
2020-07-10 17:47:22 +0200

    3 steps

--- src/components/Process.vue
+++ src/components/Process.vue
@@ -8,16 +8,43 @@
       <p class="process__desc">Daisy is sagittis sem nibh id elit. Duis sed odio sit amet nibh
         Daisy is sagittis sem nibh id elit. Duis sed odio sit amet nibh
       </p>
-      <div class="process__step1">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div class="process__photo">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div class="process__steps">
+        <div class="process__step1">
+          <div></div>
+          <div>
+            <h4>Step
+              <span>1</span>
+            </h4>
+            <p>This is Photoshop's version  of loren ipsumm. Proin</p>
+          </div>
+          <div></div>
+        </div>
+        <div class="process__step2">
+          <div></div>
+          <div>
+            <h4>Step
+              <span>2</span>
+            </h4>
+            <p>This is Photoshop's version  of loren ipsumm. Proin</p>
+          </div>
+          <div></div>
+        </div>
+        <div class="process__step3">
+          <div></div>
+          <div>
+            <h4>Step
+              <span>3</span>
+            </h4>
+            <p>This is Photoshop's version  of loren ipsumm. Proin</p>
+          </div>
+          <div></div>
+        </div>
+        <div class="process__photo">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </div>
   </section>
@@ -63,13 +90,34 @@ export default {
     width: 490px;
     margin: 20px auto;
   }
-  &__photo {
-    background-image: url('../assets/images/steps.png');
-    .hexogonimage(white, 262px, 302px);
+  &__steps {
+    position: relative;
+  }
+  &__step1, &__step2, &__step3 {
+    position: absolute;
+    .hexogontext(@itemactive, @paragraphcolor, 174px, 201px);
   }
   &__step1 {
-    position: relative;
-    //.hexogontext(red, 1px, 174px, 201px);
+    left: 5px;
+    top: 102px;
+    z-index: 20;
+  }
+  &__step2 {
+    left: 260px;
+    top: 147px;
+    z-index: 20;
+  }
+  &__step3 {
+    left: 348px;
+    top: -3px;
+    z-index: 20;
+  }
+  &__photo {
+    position: absolute;
+    top: 241px;
+    left: 49px;
+    background-image: url('../assets/images/steps.png');
+    .hexogonimage(white, 262px, 302px);
   }
 }
 </style>
