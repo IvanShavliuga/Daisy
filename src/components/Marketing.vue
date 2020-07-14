06e093ab3c202abd79f2ea85aa34fa2f30035913
Ivan <iva.drakon.nov@gmail.com>
2020-07-14 17:17:53 +0200

    Marketing update

--- src/components/Marketing.vue
+++ src/components/Marketing.vue
@@ -27,7 +27,7 @@ export default {
 <style lang="less">
 .landing {
   width: 100%;
-  height: 100vh - 10px;
+  height: 100vh - 20px;
   margin: 0;
   padding-top: 40px;
   background-color: @bodygraybg;
